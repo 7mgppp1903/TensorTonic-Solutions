a3d1655f746b2ebea1e595c7593e86b0a3fcac7b
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-03-25 22:39:00 +0530

    Add solution for matrix-normalization

---
+++ matrix-normalization/matrix-normalization.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def matrix_normalization(matrix, axis=None, norm_type='l2'):
+    try:
+        matrix = np.array(matrix, dtype=float)
+
+        if matrix.ndim > 2:
+            return None
+        
+ 
+        if matrix.ndim == 1:
+            if axis is not None and axis != 0:
+                return None
+
+       
+        if axis is not None and axis >= matrix.ndim:
+            return None
+
+        if norm_type == 'l2':
+            norm = np.sqrt(np.sum(matrix**2, axis=axis, keepdims=True))
+        elif norm_type == 'l1':
+            norm = np.sum(np.abs(matrix), axis=axis, keepdims=True)
+        elif norm_type == 'max':
+            norm = np.max(np.abs(matrix), axis=axis, keepdims=True)
+        else:
+            return None
+
+        norm = np.where(norm == 0, 1, norm)
+
+        return matrix / norm
+
+    except:
+        return None
